b85ba7ec94eb62f0b05eeb5c09fb60b93b8ef943
tw1337 <mysyanovsergey@yandex.ru>
2024-04-24 22:25:26 +0400

    hw02 review

--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoServerTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoServerTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatList;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -35,7 +36,20 @@ public class CsvQuestionDaoServerTest {
     @Test
     public void assertNoExceptionOnCorrectFile() {
         Mockito.when(appProperties.getTestFileName()).thenReturn("questions_correct_format.csv");
-        assertThatList(questionDao.findAll()).hasSize(2);
+        var questions = questionDao.findAll();
+        assertThatList(questions).hasSize(2);
+        assertThat(questions.get(0)).satisfies(question -> assertThat(question.text()).isEqualTo("Correct question one?"));
+        assertThat(questions.get(0).answers())
+                .satisfiesAnyOf(
+                        answers -> assertThat(answers.get(0).text()).isEqualTo("True"),
+                        answers -> assertThat(answers.get(1).text()).isEqualTo("False")
+                );
+        assertThat(questions.get(1)).satisfies(question -> assertThat(question.text()).isEqualTo("Correct question two?"));
+        assertThat(questions.get(1).answers())
+                .satisfiesAnyOf(
+                        answers -> assertThat(answers.get(0).text()).isEqualTo("True"),
+                        answers -> assertThat(answers.get(1).text()).isEqualTo("False")
+                );
     }
 
     @Test
--- hw02/src/test/java/ru/otus/hw/service/TestServiceImplServerTest.java
+++ hw02/src/test/java/ru/otus/hw/service/TestServiceImplServerTest.java
@@ -1,9 +1,9 @@
 package ru.otus.hw.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.hw.dao.QuestionDao;
@@ -16,6 +16,9 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatList;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import static ru.otus.hw.util.QuestionConstants.FIRST_ANSWER_NUMBER;
 import static ru.otus.hw.util.QuestionConstants.INVALID_ANSWER_NUMBER_TEMPLATE;
 
@@ -28,15 +31,11 @@ public class TestServiceImplServerTest {
     @Mock
     private QuestionDao questionDao;
 
+    @InjectMocks
     private TestServiceImpl testService;
 
-    @BeforeEach
-    public void setUp() {
-        testService = new TestServiceImpl(ioService, questionDao);
-    }
-
     @Test
-    public void testSomething() {
+    public void assertTestExecutionWithNoErrors() {
         var questions = List.of(
                 new Question("First question", List.of(new Answer("ans1", true), new Answer("ans2", false))),
                 new Question("Second question", List.of(new Answer("ans1", false), new Answer("ans2", true)))
@@ -51,6 +50,20 @@ public class TestServiceImplServerTest {
 
         var student = new Student("Albert", "Einstein");
         var testResult = testService.executeTestFor(student);
+        verify(questionDao, times(1)).findAll();
+
+        verify(ioService, times(1)).printLine("");
+        verify(ioService, times(1)).printFormattedLine("Please answer the questions below%n");
+        verify(ioService, times(1))
+                .readIntForRangeWithPrompt(1, 2,
+                        "First question:\n1) ans1\n2) ans2\n",
+                        "The answer is incorrect. The answer should be a number from 1 to 2."
+                );
+        verify(ioService, times(1))
+                .readIntForRangeWithPrompt(1, 2,
+                        "Second question:\n1) ans1\n2) ans2\n",
+                        "The answer is incorrect. The answer should be a number from 1 to 2."
+                );
         assertThat(testResult.getStudent()).isEqualTo(student);
         assertThatList(testResult.getAnsweredQuestions()).hasSize(2);
         assertThat(testResult.getRightAnswersCount()).isEqualTo(1);
